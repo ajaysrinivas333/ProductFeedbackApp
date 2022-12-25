638a602c41eef193a3618cfdd53f2507b23fc411
Ajay Srinivas A <ajaysrinivas333@gmail.com>
2022-12-25 23:01:07 +0530

    api-signup is completed

--- src/backend/models/user.ts
+++ src/backend/models/user.ts
@@ -1,26 +1,23 @@
 import { models, Schema, model } from 'mongoose';
 
 const userSchema = new Schema(
-	{
-		username: {
-			type: String,
-			required: true,
-		},
-		email: {
-			type: String,
-			required: true,
-		},
-		password: {
-			type: String,
-			required: true,
-		},
-		avatar: {
-			type: String,
-		},
-	},
-	{
-		timestamps: true,
-	},
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 const User = models?.User ?? model('User', userSchema, 'users');
--- src/components/Auth/AuthForm/index.tsx
+++ src/components/Auth/AuthForm/index.tsx
@@ -9,83 +9,95 @@ import router from 'next/router';
 import { signIn } from 'next-auth/react';
 
 interface FormData {
-	username?: string | null;
-	email: string | null;
-	password: string | null;
+  username?: string | null;
+  email: string | null;
+  password: string | null;
 }
 
 interface AuthFormProps {
-	isLogin: boolean;
+  isLogin: boolean;
 }
 
 const AuthForm = (props: AuthFormProps) => {
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-		reset,
-	} = useForm<FormData>({
-		mode: 'onTouched',
-		defaultValues: {
-			username: null,
-			email: null,
-			password: null,
-		},
-	});
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<FormData>({
+    mode: 'onTouched',
+    defaultValues: {
+      username: null,
+      email: null,
+      password: null,
+    },
+  });
 
-	const { isLogin } = props;
+  const { isLogin } = props;
 
-	const formFields = useMemo(() => getFields(isLogin), [isLogin]);
+  const formFields = useMemo(() => getFields(isLogin), [isLogin]);
 
-	useEffect(() => {
-		reset();
-	}, [reset, isLogin]);
+  useEffect(() => {
+    reset();
+  }, [reset, isLogin]);
 
-	const submitHandler = async (data: FormData) => {
-		// TODO: Call signup or signin api accordingly
+  const submitHandler = async (data: FormData) => {
+    // TODO: Call signup or signin api accordingly
 
-		if (isLogin) {
-			const res = await signIn('credentials', {
-				email: data.email,
-				password: data.password,
-				redirect: false,
-			});
-			!res?.error ? router.replace('/') : console.log(res?.error);
-		}
-	};
+    if (isLogin) {
+      const res = await signIn('credentials', {
+        email: data.email,
+        password: data.password,
+        redirect: false,
+      });
+      !res?.error ? router.replace('/') : console.log(res?.error);
 
-	return (
-		<form className={styles.authForm} onSubmit={handleSubmit(submitHandler)}>
-			{formFields.map((field) => (
-				<div key={field.id} className={styles.inputWrapper}>
-					<label htmlFor={field.id}>{field.label}</label>
-					<Input
-						inputProps={{
-							id: field.id,
-							type: field.type,
-							placeholder: field.placeholder,
-							className: styles.inputBox,
-						}}
-						helperProps={{
-							helperText: errors[field.id as keyof FormData]?.message ?? '',
-							helperTextClass: `${styles.inputBoxHelper} ${
-								errors[field.id as keyof FormData] ? styles.error : ''
-							}`,
-						}}
-						{...register(
-							field.id as keyof FormData,
-							VALIDATORS[field.id as keyof FormData],
-						)}
-					/>
-				</div>
-			))}
+      return;
+    }
 
-			<Button
-				text={isLogin ? 'Login' : 'Sign Up'}
-				className={styles.submitButton}
-			/>
-		</form>
-	);
+    const res = await fetch('/api/auth/signup', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    console.log(await res.json());
+  };
+
+  return (
+    <form className={styles.authForm} onSubmit={handleSubmit(submitHandler)}>
+      {formFields.map((field) => (
+        <div key={field.id} className={styles.inputWrapper}>
+          <label htmlFor={field.id}>{field.label}</label>
+          <Input
+            inputProps={{
+              id: field.id,
+              type: field.type,
+              placeholder: field.placeholder,
+              className: styles.inputBox,
+            }}
+            helperProps={{
+              helperText: errors[field.id as keyof FormData]?.message ?? '',
+              helperTextClass: `${styles.inputBoxHelper} ${
+                errors[field.id as keyof FormData] ? styles.error : ''
+              }`,
+            }}
+            {...register(
+              field.id as keyof FormData,
+              VALIDATORS[field.id as keyof FormData]
+            )}
+          />
+        </div>
+      ))}
+
+      <Button
+        text={isLogin ? 'Login' : 'Sign Up'}
+        className={styles.submitButton}
+      />
+    </form>
+  );
 };
 
 export default AuthForm;
