12657ea4f87f1eba971c94c7e33c2ac11b4e82c7
Ajay Srinivas A <85863604+ajaysrinivas333@users.noreply.github.com>
2022-12-25 23:03:53 +0530

    api-signup is completed (#4)

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,20 +5,20 @@ import User from '../../../backend/models/user';
 import connectDb from '../../../backend/db/connection';
 
 export default NextAuth({
-	providers: [
-		CredentialsProvider({
-			name: 'credentials',
-			credentials: {
-				email: {},
-				password: {},
-			},
-			async authorize(
-				credentials: Record<'email' | 'password', string> | undefined,
-			) {
-				try {
-					await connectDb();
-					//  Uncomment to test login with test credentials
-					/* 
+  providers: [
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(
+        credentials: Record<'email' | 'password', string> | undefined
+      ) {
+        try {
+          await connectDb();
+          //  Uncomment to test login with test credentials
+          /* 
 					if (
 						credentials?.email === 'test@test.com' &&
 						credentials?.password === 'password'
@@ -31,56 +31,55 @@ export default NextAuth({
 						};
 					 }
 					*/
-					const user = await User.findOne({
-						email: credentials?.email,
-					});
+          const user = await User.findOne({
+            email: credentials?.email,
+          });
 
-					if (!user) throw new Error("User doesn't exist");
+          if (!user) throw new Error("User doesn't exist");
 
-					const isPasswordMatch = await bcrypt.compare(
-						credentials?.password as string,
-						user.password,
-					);
+          const isPasswordMatch = await bcrypt.compare(
+            credentials?.password as string,
+            user.password
+          );
 
-					if (!isPasswordMatch) throw new Error('Invalid credentials');
+          if (!isPasswordMatch) throw new Error('Invalid credentials');
 
-					return {
-						id: user._id.toString(),
-						email: user.email,
-						image: user.avatar,
-						name: user.userName,
-					};
-				} catch (err) {
-					throw err;
-				}
-			},
-		}),
-	],
-	pages: {
-		signIn: '/auth',
-	},
-	callbacks: {
-		async jwt(data) {
-			if (data.user) {
-				data.token['id'] = data.user.id;
-			}
-			return data.token;
-		},
+          return {
+            id: user._id.toString(),
+            email: user.email,
+            name: user.userName,
+          };
+        } catch (err) {
+          throw err;
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/auth',
+  },
+  callbacks: {
+    async jwt(data) {
+      if (data.user) {
+        data.token['id'] = data.user.id;
+      }
+      return data.token;
+    },
 
-		async session(data) {
-			if (data.session.user && data.token) {
-				data.session.user.id = data.token.id;
-			}
-			return data.session;
-		},
-	},
-	secret: process.env.NEXTAUTH_SECRET,
-	session: {
-		maxAge: 60 * 60,
-		/**
-		 * How often the session should be updated in seconds.
-		 * If set to `0`, session is updated every time.
-		 */
-		updateAge: 60 * 50,
-	},
+    async session(data) {
+      if (data.session.user && data.token) {
+        data.session.user.id = data.token.id;
+      }
+      return data.session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    maxAge: 60 * 60,
+    /**
+     * How often the session should be updated in seconds.
+     * If set to `0`, session is updated every time.
+     */
+    updateAge: 60 * 50,
+  },
 });
