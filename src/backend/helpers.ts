f91de25e4837e1956cd46ea1c6f8545129ff6711
Ajay Srinivas A <ajaysrinivas333@gmail.com>
2023-01-01 16:23:16 +0530

    Added Edit Product API

--- src/backend/helpers.ts
+++ src/backend/helpers.ts
@@ -2,10 +2,13 @@ import { NextApiRequest } from 'next';
 import { getToken } from 'next-auth/jwt';
 
 export const isAuthenticated = async (
-	req: NextApiRequest,
+  req: NextApiRequest
 ): Promise<string | null> => {
-	const token = await getToken({ req });
-	return token ? token.id! : null;
+  const token = await getToken({ req });
+  return token ? token.id! : null;
 };
 
 export const isEmpty = (s: string) => !s?.length || s?.trim()?.length <= 0;
+
+export const hasKey = <T extends Record<string, any>>(object: T, key: string) =>
+  object.hasOwnProperty(key);
