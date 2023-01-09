b7f1c59e602a12f861f1289408c46ba016e43814
Varadarajan M <varad2k12@gmail.com>
2023-01-10 00:12:12 +0530

    Completed add product functionality

--- src/components/UI/Input.tsx
+++ src/components/UI/Input.tsx
@@ -1,29 +1,37 @@
 import React, { Fragment } from 'react';
 
 interface InputProps {
-  helperProps?: { helperText?: string; helperTextClass?: string };
-  inputProps: {
-    className?: string;
-    placeholder?: string;
-    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
-    style?: React.CSSProperties;
-    id?: string;
-    type?: string;
-  };
+	helperProps?: { helperText?: string; helperTextClass?: string };
+	inputProps: {
+		className?: string;
+		placeholder?: string;
+		onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+		style?: React.CSSProperties;
+		id?: string;
+		type?: string;
+	};
 }
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  (props: InputProps, ref) => {
-    const { helperProps, inputProps, ...restProps } = props;
-    return (
-      <Fragment>
-        <input {...inputProps} {...restProps} ref={ref} />
-        <br />
-        <small className={helperProps?.helperTextClass}>
-          {helperProps?.helperText}
-        </small>
-      </Fragment>
-    );
-  }
+	(props: InputProps, ref) => {
+		const { helperProps, inputProps, ...restProps } = props;
+		const inputClasses = `${inputProps.className} customInputBox`;
+		const helperTextClasses = `${helperProps?.helperTextClass} customInputBoxHelperText`;
+		return (
+			<div className='customInputWrapper'>
+				<input
+					{...inputProps}
+					{...restProps}
+					className={inputClasses}
+					ref={ref}
+				/>
+				{helperProps?.helperText ? (
+					<small className={helperTextClasses}>{helperProps?.helperText}</small>
+				) : (
+					''
+				)}
+			</div>
+		);
+	},
 );
 
 Input.displayName = 'Input';
--- src/pages/add-product/index.tsx
+++ src/pages/add-product/index.tsx
@@ -1,79 +1,46 @@
 import Card from 'components/UI/Card';
 import React from 'react';
 import styles from '../../styles/addproduct.module.scss';
-import Input from 'components/UI/Input';
-import { useForm } from 'react-hook-form';
 import Button from 'components/UI/Button';
 import { BiChevronLeft } from 'react-icons/bi';
-import { BsChevronDown, BsPlusCircleFill } from 'react-icons/bs';
-import { Menu, MenuContainer, MenuItem } from 'components/UI/Menu';
-import { productCategories } from 'lib/constants';
-import { TiTick } from 'react-icons/ti';
-import useMenu from '../../hooks/use-menu';
+import { BsPlusCircleFill } from 'react-icons/bs';
+import ProductForm from 'components/Product/ProductForm';
+import { GetServerSideProps } from 'next';
+import { getSession } from 'next-auth/react';
+import Link from 'next/link';
 
 const AddProductPage = () => {
-	const { open, openMenu, closeMenu } = useMenu();
-	const { handleSubmit } = useForm();
 	return (
 		<div className={styles.addProductContainer}>
 			<div className={styles.innerContainer}>
 				<div className={styles.backButtonWrapper}>
 					<BiChevronLeft className={styles.backIcon} />
-					<Button className={styles.backButton} text='Go Back' />
+					<Link style={{ width: '100%' }} href={'/home'}>
+						<Button className={styles.backButton} text='Go Back' />
+					</Link>
 				</div>
 				<Card className={styles.addProductCard}>
 					<BsPlusCircleFill className={styles.plusButton} />
-					<form onSubmit={handleSubmit((d) => console.log(d))}>
-						<div className={styles.addProductWrapper}>
-							<header className={styles.header}>Create New Product</header>
-							<span className={styles.fieldName}>Product Title</span>
-							<label>Add a short, descriptive headline</label>
-							<Input
-								inputProps={{
-									type: 'text',
-									placeholder: 'Product Description',
-									className: styles.inputBox,
-								}}
-							/>
-							<span className={styles.fieldName}>Category</span>
-							<label>Choose a category for your Product</label>
-
-							<MenuContainer className={styles.dropdownContainer}>
-								<button className={styles.dropdown} onClick={openMenu}>
-									<span>{'Category'}</span>
-									<BsChevronDown />
-								</button>
-								<Menu className={styles.menu} open={open} onBlur={closeMenu}>
-									{productCategories.map((category) => (
-										<MenuItem
-											className={styles.menuItem}
-											onClick={() => {}}
-											key={category}
-										>
-											{category}
-											<span className={'icon'}>{<TiTick />}</span>
-										</MenuItem>
-									))}
-								</Menu>
-							</MenuContainer>
-
-							<span className={styles.fieldName}>Product Detail</span>
-							<label>
-								Include any specific details on what the Product is used for,
-								etc.
-							</label>
-
-							<textarea className={styles.textArea}></textarea>
-							<div className={styles.buttonLayout}>
-								<Button className={styles.cancelButton} text='Cancel' />
-								<Button className={styles.submitButton} text='Add Product' />
-							</div>
-						</div>
-					</form>
+					<ProductForm mode='create' />
 				</Card>
 			</div>
 		</div>
 	);
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+	const session = await getSession(context);
+	if (!session) {
+		return {
+			redirect: {
+				destination: '/',
+				permanent: false,
+			},
+		};
+	}
+	return {
+		props: {},
+	};
 };
 
 export default AddProductPage;
