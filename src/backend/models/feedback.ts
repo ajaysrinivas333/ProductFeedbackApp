d0de8c53dc92dc24bed21c077fb7ad820a164fff
Varadarajan M <varad2k12@gmail.com>
2023-01-12 00:07:33 +0530

    Added new feedback api

---
+++ src/backend/models/feedback.ts
@@ -0,0 +1,60 @@
+import mongoose, { models, Schema, model } from 'mongoose';
+import { FEEDBACK_STATUS, FEEDBACK_CATEGORIES } from '@api/constants';
+
+const feedbackSchema = new Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		description: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		category: {
+			type: String,
+			required: true,
+			enum: FEEDBACK_CATEGORIES,
+		},
+		upvotesCount: {
+			type: Number,
+			default: 0,
+		},
+		upvotedUsers: {
+			type: [mongoose.Schema.Types.ObjectId],
+			ref: 'User',
+		},
+		commentsCount: {
+			type: Number,
+			default: 0,
+		},
+		userId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+		productId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Product',
+			required: true,
+		},
+		position: {
+			type: Number,
+		},
+		status: {
+			type: String,
+			enum: FEEDBACK_STATUS,
+			default: FEEDBACK_STATUS.REQUESTED,
+		},
+	},
+	{
+		timestamps: true,
+	},
+);
+
+const Feedback =
+	models?.Feedback ?? model('Feedback', feedbackSchema, 'feedbacks');
+
+export default Feedback;
