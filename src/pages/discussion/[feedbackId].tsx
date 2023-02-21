88e4cebcd2296473f55b06b120c4e0dbbade2dbe
Varadarajan M <varad2k12@gmail.com>
2023-02-21 18:40:27 +0530

    Extracted upvote/downvote functionality into custom hook, linked discussions page, added upvote/downvote via discussions page.

--- src/pages/discussion/[feedbackId].tsx
+++ src/pages/discussion/[feedbackId].tsx
@@ -1,51 +1,83 @@
+import connectDb from '@api/db/connection';
+import { findFeedbacksWithUserDetails } from '@api/helpers';
 import AddCommentForm from 'components/Comments/AddCommentForm';
 import CommentSection from 'components/Comments/CommentSection';
-import FeedbackCard from 'components/Feedback/FeedbackCard';
+import FeedbackCard, { Feedback } from 'components/Feedback/FeedbackCard';
 import Button from 'components/UI/Button';
+import GlobalLoader from 'components/UI/GlobalLoader';
+import useAuth from 'hooks/use-auth';
+import useUpvote from 'hooks/use-upvote';
+import { Types } from 'mongoose';
+import { GetStaticProps } from 'next';
+import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import { BiChevronLeft } from 'react-icons/bi';
 import styles from '../../styles/discussion-page.module.scss';
 
-const dummyFeedback = {
-	_id: '63c973e3b3ec4e81af4c9c73',
-	title: 'Make comments editable',
-	description:
-		"Users should be able to edit their comments. This isn't Twitter 😕",
-	category: 'Feature',
-	upvotesCount: 2,
-	upvotedUsers: ['63bdbba7321830e0cf4f7a20', '63bdaebb321830e0cf4f79fc'],
-	commentsCount: 0,
-	userId: '63bdaebb321830e0cf4f79fc',
-	productId: '63bdbe29321830e0cf4f7a24',
-	createdAt: '2023-01-19T16:46:27.869Z',
-	updatedAt: '2023-02-18T11:14:27.926Z',
-	user: {
-		_id: '63bdaebb321830e0cf4f79fc',
-		username: 'Varadarajan M',
-		email: 'varad2k12@gmail.com',
-		__v: 0,
-	},
+
+type DiscussionPageProps = {
+	feedback: Feedback[];
 };
 
-const DiscussionPage = () => {
+const DiscussionPage = (props: DiscussionPageProps) => {
 	const router = useRouter();
+	const { isAuthenticated } = useAuth();
+	const [feedbackData, setFeedbackData] = useState(props?.feedback ?? []);
+	const { isUpvoted, upvote, downvote, upvoteApi } = useUpvote(
+		feedbackData,
+		setFeedbackData,
+	);
+	const { data: session } = useSession();
+
+	const { productId } = router?.query ?? '';
+
+	useEffect(() => {
+		setFeedbackData(props.feedback);
+	}, [props.feedback]);
+
+	const onUpvote = useCallback(
+		async (feedbackId: string) => {
+			if (isAuthenticated) {
+				isUpvoted(feedbackId) ? downvote(feedbackId) : upvote(feedbackId);
+				const res = await upvoteApi(feedbackId, productId as string);
+				return;
+			}
+
+			router.replace('/auth');
+		},
+		[
+			downvote,
+			isAuthenticated,
+			isUpvoted,
+			productId,
+			router,
+			upvote,
+			upvoteApi,
+		],
+	);
+
+	if (router.isFallback) return <GlobalLoader />;
 
 	return (
 		<div className={styles.pageWrapper}>
 			<div className={styles.innerContainer}>
 				<div className={styles.backButtonWrapper}>
 					<BiChevronLeft className={styles.backIcon} />
-					<Link style={{ width: '100%' }} href={'/home'}>
+					<Link style={{ width: '100%' }} href={`/feedbacks/${productId}`}>
 						<Button className={styles.backButton} text='Go Back' />
 					</Link>
 				</div>
-				<FeedbackCard
-					feedback={dummyFeedback}
-					isUpvoted
-					onUpvote={(s) => console.log(s)}
-				/>
+				{feedbackData && (
+					<FeedbackCard
+						feedback={feedbackData[0] ?? {}}
+						isUpvoted={feedbackData[0]?.upvotedUsers?.includes(
+							session?.user?.id as string,
+						)}
+						onUpvote={onUpvote}
+					/>
+				)}
 
 				<AddCommentForm className={styles.commentForm} />
 
@@ -53,6 +85,32 @@ const DiscussionPage = () => {
 			</div>
 		</div>
 	);
+};
+
+export const getStaticPaths = () => {
+	return {
+		paths: [],
+		fallback: true,
+	};
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+	let feedback;
+	try {
+		await connectDb();
+		feedback = await findFeedbacksWithUserDetails({
+			_id: new Types.ObjectId(ctx.params?.feedbackId as string),
+		});
+	} catch (error: any) {
+		console.log(error?.message);
+	} finally {
+		return {
+			props: {
+				feedback: JSON.parse(JSON.stringify(feedback)),
+			},
+			revalidate: 2,
+		};
+	}
 };
 
 export default DiscussionPage;
