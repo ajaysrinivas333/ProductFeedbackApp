7a92a95941a6780531ae7138d8ce7a751d10b15b
Ajay Srinivas A <ajaysrinivas333@gmail.com>
2023-03-22 23:47:06 +0530

    Added Deployent configs

--- src/backend/db/connection.ts
+++ src/backend/db/connection.ts
@@ -1,13 +1,15 @@
 import mongoose from 'mongoose';
 
 const connectDb = async (): Promise<void> => {
-  mongoose.set('strictQuery', true);
-  mongoose.connect('mongodb://localhost:27017/product-feedback-app', (err) => {
-    if (err) console.log('Error has occured :', err.message);
-  });
-  mongoose.connection.on('connected', () => {
-    console.log('connected to database');
-  });
+	mongoose.set('strictQuery', true);
+	const mongoUri =
+		process.env.MONGO_URI || 'mongodb://localhost:27017/product-feedback-app';
+	mongoose.connect(mongoUri, (err) => {
+		if (err) console.log('Error has occured :', err.message);
+	});
+	mongoose.connection.on('connected', () => {
+		console.log('connected to database');
+	});
 };
 
 export default connectDb;
